381509f68e8acc9829a996bafd6db2e78f4af9f6
Juanleon-19 <jleon.saiz888@gmail.com>
2026-07-30 23:52:17 -0500

    fix(height-map): apply phase 3 mesh state patch

--- src/klipper_cnc_assistant/application/__init__.py
+++ src/klipper_cnc_assistant/application/__init__.py
@@ -2,9 +2,12 @@ from .compensated_gcode_service import CompensatedGCodeService
 from .errors import ApplicationError, NotFoundError
 from .heightmap_service import HeightMapService
 from .physical_map_service import PhysicalMapService
+from .physical_map_phase3_patch import apply_phase3_patch
 from .reference_service import ReferenceSessionService
 from .services import MachineSessionService, ProjectService, SystemStatusService
 from klipper_cnc_assistant.execution import JobService, MeshExecutionService
+
+apply_phase3_patch(PhysicalMapService)
 
 __all__ = [
     "ApplicationError",
